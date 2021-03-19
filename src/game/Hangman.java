69712e92c9f8f4824a584ce703928420c0762330
pc <pc@DESKTOP-6JCS8QP>
2021-03-19 14:06:00 +0100

    HANGMAN GAME GUI Version of Hangman Application created during PGD Software Engineering Course at NIIT Abuja

---
+++ src/game/Hangman.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author pc
+ */
+public class Hangman {
+    public static void main(String[] args){
+        Menu mObject = new Menu();
+        mObject.addComponent();
+    }
+}
